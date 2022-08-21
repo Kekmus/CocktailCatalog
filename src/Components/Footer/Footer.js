718df08e45886f02a4e67dc5a1489b1ad37de620
Kekmus <lgstrwrs@gmail.com>
2022-08-21 15:05:28 +0500

    Fix: add taget blank on footer link

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,18 @@
-import style from"./Footer.module.css"
+import style from "./Footer.module.css";
 
 function Footer() {
   return (
     <footer className={style.footer__wrapper}>
-      <a className={style.github__link} href="https://github.com/Kekmus/CocktailCatalog">Github repo</a>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className={style.github__link}
+        href="https://github.com/Kekmus/CocktailCatalog"
+      >
+        Github repo
+      </a>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
